9494945a5057f2068adc49fe101db8b15da13fb5
harshit gahlaut <harshit.gahlaut2005@gmail.com>
2025-03-16 01:37:22 +0530

    sports quiz , fetched api and answers and checked correctly

---
+++ AllQuizzes/quiz_script.js
@@ -0,0 +1,49 @@
+console.log("starting");
+const URL = "https://opentdb.com/api.php?amount=10&category=21&difficulty=medium&type=multiple";
+let results = [];
+let correctAnswers = [];
+function decodeHtmlEntities(text) {
+    let parser = new DOMParser();
+    let decodedString = parser.parseFromString(text, "text/html").body.textContent;
+    return decodedString;
+}
+async function fetching(URL) {
+    let response = await fetch(URL);
+    let data = await response.json();
+    results = data.results;
+    correctAnswers = results.map(result => result.correct_answer);
+    QuesDisplay();
+    AnswerDisplay();
+}
+function QuesDisplay() {
+    for (ques in results) {
+        let question = document.querySelector(`.question-${ques}`);
+        question.textContent = decodeHtmlEntities(results[ques].question);
+    }
+}
+function AnswerDisplay() {
+    for (ques in results) {
+        let answerArray = results[ques].incorrect_answers.concat(results[ques].correct_answer);
+        answerArray.sort(() => Math.random() - 0.5);
+        for (let i = 0; i < 4; i++) {
+            let answer = document.querySelector(`#LabelAnswer${ques}-${i + 1}`);
+            let RadioVal = document.querySelector(`#answer${ques}-${i + 1}`);
+            answer.textContent = decodeHtmlEntities(answerArray[i]);
+            RadioVal.value = answerArray[i];
+        }
+    }
+}
+// submit button functionality
+
+fetching(URL);
+document.querySelector(".btn").addEventListener("click", () => {
+    let score = 0;
+    for (let ques in results) {
+        let userAnswer = document.querySelector(`input[name="answer${ques}"]:checked`).value;
+        if (userAnswer === correctAnswers[ques]) {
+            score++;
+        }
+    }
+    alert(`Your Score is ${score}`);
+
+});
